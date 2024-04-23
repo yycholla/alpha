b27cadc8e9a42fea002f254bbf6113329a41ff8c
yycholla <hanway.colin@gmail.com>
2024-04-23 11:14:29 -0600

    update paths

--- Staging.ps1
+++ Staging.ps1
@@ -4,7 +4,7 @@
 # $WhatIfPreference = $True
 
 # Test if Temp exists create if it doesn't
-$tempPath = Test-Path -Path "C:\Temp"
+$tempPath = Test-Path -Path "C:\Temp\alpha"
 $TestChromeInstalled = Test-Path -Path "C:\Program Files\Google\Chrome"
 $TestDriveInstalled = Test-Path -Path "C:\Program Files\Google\Drive File Stream"
 $TestGCPWInstalled = Test-Path -Path "C:\Program Files\Google\Credential Provider"
@@ -14,13 +14,13 @@ $TestSymantecInstalled = Test-Path -Path "C:\Program Files\Symantec\Symantec End
 # Test if C:\Temp exists, create if no
 if($tempPath) {
     write-output "`nTemp already exists`n" -ForegroundColor Green
-    if (Test-Path -Path "C:\Temp\ChromeInstaller.exe") {
+    if (Test-Path -Path "C:\Temp\alpha\ChromeInstaller.exe") {
         $downloadChrome = $False
     } else { $downloadChrome = $True }
-    if (Test-Path -Path "C:\Temp\gcpwstandaloneenterprise64.msi") {
+    if (Test-Path -Path "C:\Temp\alpha\gcpwstandaloneenterprise64.msi") {
         $downloadGCPW = $False
     } else { $downloadGCPW = $True }
-    if (Test-Path -Path "C:\Temp\GoogleDriveSetup.exe") {
+    if (Test-Path -Path "C:\Temp\alpha\GoogleDriveSetup.exe") {
         $downloadDrive = $False
     } else { $downloadDrive = $True }
     if ($downloadChrome -and $downloadGCPW -and $downloadDrive) {
@@ -39,7 +39,7 @@ function InstallChrome {
     try {
         if ($TestChromeInstalled -eq $False) {
             Write-Verbose "Running Google Chrome setup."
-            $setupFile = "C:\Temp\ChromeInstaller.exe"
+            $setupFile = "C:\Temp\alpha\ChromeInstaller.exe"
             Start-Process $setupFile -Verb runAs -ArgumentList '/silent /install'
         } else {Write-Host "Chrome is already installed" -ForegroundColor Green}
     }
@@ -51,7 +51,7 @@ function InstallDrive {
     try {
         if ($TestDriveInstalled -eq $False) {
             Write-Verbose "Running Google Drive setup."
-            $setupFile = "C:\Temp\GoogleDriveSetup.exe"
+            $setupFile = "C:\Temp\alpha\GoogleDriveSetup.exe"
             Start-Process $setupFile -Verb runAs -ArgumentList '--silent --desktop_shortcut --gsuite_shortcuts=false'
         } else {Write-Host "Drive is already installed" -ForegroundColor Green}
     }
@@ -64,7 +64,7 @@ function InstallGCPW {
     try {
         if ($TestGCPWInstalled -eq $False) {
             Write-Verbose "Running GCPW setup."
-            $setupFile = "c:\Temp\gcpwstandaloneenterprise64.msi"
+            $setupFile = "c:\Temp\alpha\gcpwstandaloneenterprise64.msi"
             $params = '/i', $setupFile, '/quiet'
             Start-Process msiexec -Verb runAs -wait -ArgumentList $params
         } else {Write-Host "GCPW is already installed" -ForegroundColor Green}
@@ -78,24 +78,24 @@ function InstallNAble {
     try {
         if ($TestNableInstalled -eq $False) {
             if ($nAble -eq "y") {
-                if (Test-Path -path "C:\Temp\nAble.exe") {
+                if (Test-Path -path "C:\Temp\alpha\nAble.exe") {
                     Write-Host "`nInstalling N-Able"
-                    Start-Process C:\Temp\nAble.exe -ArgumentList '/s " /qn"' -Wait
+                    Start-Process C:\Temp\alpha\nAble.exe -ArgumentList '/s " /qn"' -Wait
                     Write-Host "N-Able Installation Completed!"
                 } else {
                     Write-Host = "Installing N-Able"
 
                     $uri = 'https://ncentral.alphamediausa.com/download/current/winnt/N-central/WindowsAgentSetup.exe'
-                    Invoke-Webrequest -uri $uri -OutFile "C:\Temp\WindowsAgentSetup.exe"
+                    Invoke-Webrequest -uri $uri -OutFile "C:\Temp\alpha\WindowsAgentSetup.exe"
 
-                    Import-Csv 'C:\Temp\Markets.csv' | ft
+                    Import-Csv 'C:\Temp\alpha\Markets.csv' | ft
 
                     $search = Read-Host "Enter Market"
                     $regKey = Read-Host "Enter registration key"
                     $siteID = ""
 
                     while ($siteID -eq "") {
-                        Import-Csv C:\Temp\SiteID.csv | Where-Object { $_.Customer -eq $search } | ft
+                        Import-Csv C:\Temp\alpha\SiteID.csv | Where-Object { $_.Customer -eq $search } | ft
 
                         $siteID = Read-Host "`nEnter ID"
                         Write-Host $siteID
@@ -112,7 +112,7 @@ function InstallNAble {
                         'SERVERADDRESS=ncentral.alphamediausa.com',
                         'SERVERPORT=443"'
                     )
-                    Start-Process "C:\Temp\WindowsAgentSetup.exe" -ArgumentList $arguments -Wait -WindowStyle Hidden
+                    Start-Process "C:\Temp\alpha\WindowsAgentSetup.exe" -ArgumentList $arguments -Wait -WindowStyle Hidden
                     Write-Host = "N-Able Installation Completed!"
                 }
             }
@@ -127,9 +127,9 @@ function InstallSymantec {
     try {
         if ($TestSymantecInstalled -eq $False) {
             if ($sym -eq "y") {
-                if (Test-Path -Path "C:\Temp\Symantec_Agent_Setup.exe") {
+                if (Test-Path -Path "C:\Temp\alpha\Symantec_Agent_Setup.exe") {
                     Write-Host "`nInstalling Symantec" | Green
-                    Start-Process "C:\Temp\Symantec_Agent_Setup.exe" -Verb runAs -Wait
+                    Start-Process "C:\Temp\alpha\Symantec_Agent_Setup.exe" -Verb runAs -Wait
                     Write-Host "Symantec Installation Completed!"
                 } else {
                     Write-Host "Symantec_Agent_Setup.exe not found" -ForegroundColor Red
@@ -160,15 +160,15 @@ $GCPWPath = ""
 
 if ($downloadChrome) {
     $ChromeURL = "https://dl.google.com/chrome/install/current/chrome_installer.exe"
-    $ChromePath = "C:\Temp\ChromeInstaller.exe"
+    $ChromePath = "C:\Temp\alpha\ChromeInstaller.exe"
 }
 if ($downloadDrive) {
     $DriveURL = "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
-    $DrivePath = "C:\Temp\GoogleDriveSetup.exe"
+    $DrivePath = "C:\Temp\alpha\GoogleDriveSetup.exe"
 }
 if ($downloadGCPW) {
     $GCPWURL = "https://dl.google.com/credentialprovider/gcpwstandaloneenterprise64.msi"
-    $GCPWPath = "C:\Temp\gcpwstandaloneenterprise64.msi"
+    $GCPWPath = "C:\Temp\alpha\gcpwstandaloneenterprise64.msi"
 }
 
 if ($download) {
